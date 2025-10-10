3c985a6a603eb58428f6767b25551363ae0546fc
raheemshaik <raheemshaik2404@gmail.com>
2025-10-11 01:20:19 +0530

    ij

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project}/${var.environment}"
+  name = "/${var.project}/${var.environment}/vpc_id"
 }
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
@@ -9,7 +9,7 @@ data "aws_ssm_parameter" "private_subnet_id" {
   name = "/${var.project}/${var.environment}/private_subnet_id"
 }
 data "aws_ssm_parameter" "sg_id" {
-  name = "/${var.project}/${var.environment}/${var.component}_sg_id"
+  name = "/${var.project}/${var.environment}/${var.component}/sg_id"
 }
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
