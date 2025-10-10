3306b11e09e2ef8ea933f115ffabd5cde1eb564c
raheemshaik <raheemshaik2404@gmail.com>
2025-10-11 01:08:51 +0530

    ij

--- data.tf
+++ data.tf
@@ -5,6 +5,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
   
 }
+data "aws_ssm_parameter" "private_subnet_id" {
+  name = "/${var.project}/${var.environment}/private_subnet_id"
+}
 data "aws_ssm_parameter" "sg_id" {
   name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
--- locals.tf
+++ locals.tf
@@ -5,7 +5,10 @@ locals {
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   sg_id = data.aws_ssm_parameter.sg_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+
+
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+
 
   rule_header_url =  "${var.component}" == "frontend" ? "${var.environment}.${var.zone_name}" : "${var.component}.backend-${var.environment}.${var.zone_name}"
   aws_lb_listener_rule_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn.value : local.backend_alb_listener_arn.value
