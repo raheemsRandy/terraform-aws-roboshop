ed1899d7107898522c9c3bfba4de4e38da0821d5
raheemshaik <raheemshaik2404@gmail.com>
2025-10-11 00:33:18 +0530

    ij

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   sg_id = data.aws_ssm_parameter.sg_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
