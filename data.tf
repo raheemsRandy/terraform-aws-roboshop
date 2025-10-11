5ec9deaa016373aca5edd5976432183a1c4c6d02
raheemshaik <raheemshaik2404@gmail.com>
2025-10-11 16:20:33 +0000

    ij

--- data.tf
+++ data.tf
@@ -5,9 +5,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
   
 }
-data "aws_ssm_parameter" "private_subnet_id" {
-  name = "/${var.project}/${var.environment}/private_subnet_id"
-}
+
 data "aws_ssm_parameter" "sg_id" {
   name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
--- locals.tf
+++ locals.tf
@@ -1,14 +1,12 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   sg_id = data.aws_ssm_parameter.sg_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 
-
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-
 
   rule_header_url =  "${var.component}" == "frontend" ? "${var.environment}.${var.zone_name}" : "${var.component}.backend-${var.environment}.${var.zone_name}"
   aws_lb_listener_rule_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn.value : local.backend_alb_listener_arn.value
